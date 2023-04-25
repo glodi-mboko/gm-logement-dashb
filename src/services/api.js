e7772c0459c806b04ee9c94d4277c43ac410310f
glodi-mboko <gmboko664@gmail.com>
2023-04-25 06:09:25 +0100

    ref:change proce.env.node_env for prod

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,13 @@
 import Axios from "axios";
 
 const userToken = localStorage.getItem("mosali");
+const url =
+  process.env.NODE_ENV === "production"
+    ? process.env.REACT_APP_BASE_URL_PROD
+    : process.env.REACT_APP_BASE_URL;
 
 const instance = Axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: url,
   headers: {
     Accept: "application/json, text/plain, */*",
     "Content-type": "application/json",
